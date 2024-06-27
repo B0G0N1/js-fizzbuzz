e5bd0f28b76696f888eb25b81371bf1a142ee76a
B0G0N1 <michelebogoni2001@gmail.com>
2024-06-28 00:09:31 +0200

    [UPD] Fix JS and CSS, FINISH BONUS 1

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
+// Crea e assegna all costante chessboard il primo elemento del DOM che ha la classe chessboard
+const chessboard = document.querySelector('.chessboard')
+
 // Entro in un ciclo for che va da 1 a 100
 for (let i = 1; i <= 100; i++) {
     // Creare elemento div classe "square"
     const square = document.createElement('div');
+    square.classList.add('square');
     
     // Se il numero è multiplo di 15 allora stampo "FizzBuzz"
     if (i % 15 === 0) {
         // Aggiunge all'interno di "square" il testo "FizzBuzz"
         square.innerText = 'FizzBuzz';
     }
-    // Se il numero è multiplo di 15 allora stampo "Fizz"
+    // Se il numero è multiplo di 3 allora stampo "Fizz"
     else if (i % 3 === 0) {
         // Aggiunge all'interno di "square" il testo "Fizz"
         square.innerText = 'Fizz';
     }
-    // Se il numero è multiplo di 15 allora stampo "Buzz"
+    // Se il numero è multiplo di 5 allora stampo "Buzz"
     else if (i % 5 === 0) {
         // Aggiunge all'interno di "square" il testo "Buzz"
         square.innerText = 'Buzz';
@@ -21,7 +25,7 @@ for (let i = 1; i <= 100; i++) {
     // Altrimenti stampo il numero
     else {
         // Aggiunge all'interno di "square" il numero del ciclo
-        square.innerText = '${i}';
+        square.innerText = `${i}`;
     }
 
     // Aggiunge l'elemento "square" alla fine dell'elemento "chessboard"
