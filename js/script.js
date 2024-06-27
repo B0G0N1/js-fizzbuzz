adbeeb417515e6e48ea601340c0ebdeaaab84498
B0G0N1 <michelebogoni2001@gmail.com>
2024-06-27 23:49:38 +0200

    [FEAT] Add some base CSS style and comment/fix Javascript code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
+// Entro in un ciclo for che va da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
+    // Se il numero è multiplo di 15 allora stampo "FizzBuzz"
+    if (i % 15 === 0) {
         console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
+    }
+    // Se il numero è multiplo di 15 allora stampo "Fizz"
+    else if (i % 3 === 0) {
         console.log("Fizz");
-    } else if (i % 5 === 0) {
+    }
+    // Se il numero è multiplo di 15 allora stampo "Buzz"
+    else if (i % 5 === 0) {
         console.log("Buzz");
-    } else {
+    }
+    // Altrimenti stampo il numero
+    else {
         console.log(i);
     }
 }
