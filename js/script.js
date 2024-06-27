c3e717a687b8a5415013d4f7153e9b80d5ab6e8e
B0G0N1 <michelebogoni2001@gmail.com>
2024-06-28 00:27:09 +0200

    [FEAT] Add square color JS and CSS

--- js/script.js
+++ js/script.js
@@ -11,20 +11,24 @@ for (let i = 1; i <= 100; i++) {
     if (i % 15 === 0) {
         // Aggiunge all'interno di "square" il testo "FizzBuzz"
         square.innerText = 'FizzBuzz';
+        square.classList.add('fizzBuzz');
     }
     // Se il numero è multiplo di 3 allora stampo "Fizz"
     else if (i % 3 === 0) {
         // Aggiunge all'interno di "square" il testo "Fizz"
         square.innerText = 'Fizz';
+        square.classList.add('fizz');
     }
     // Se il numero è multiplo di 5 allora stampo "Buzz"
     else if (i % 5 === 0) {
         // Aggiunge all'interno di "square" il testo "Buzz"
         square.innerText = 'Buzz';
+        square.classList.add('buzz');
     }
     // Altrimenti stampo il numero
     else {
         // Aggiunge all'interno di "square" il numero del ciclo
+        square.classList.add('number');
         square.innerText = `${i}`;
     }
 
